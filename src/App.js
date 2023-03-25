166442c9c35945d3f5f4e982b7a34f975769dfe3
jiylee <jjenny4711@gmail.com>
2023-03-24 21:13:30 -0400

    guessgame

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Play from './pages/Play';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>숫자 맞추기 게임!!</h1>
+      <Play/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Play.js
@@ -0,0 +1,122 @@
+import { useState, useEffect, useRef } from "react";
+import '../App.css'
+import React from "react";
+
+const Play = () => {
+  const [userNum, setUserNum] = useState("");
+  const [randomNum, setRandomNum] = useState(0);
+  const [less, setLess] = useState("");
+  const [msg, setMsg] = useState("");
+  const [src,setSrc]=useState("https://media.giphy.com/media/PKxsxsl3aOH1ihZ3Um/giphy.gif")
+  const [gameOver, setGameOver] = useState(false);
+  const [chance, setChance] = useState(5);
+  const [history, setHistory] = useState([]);
+  const [dup,setDup]=useState("");
+  const focusInput = useRef();
+  const makeFocus = () => (focusInput.current.value = "");
+ console.log(msg)
+// get Random Number
+  const makeRandom = () => {
+    let random = Math.floor(Math.random() * 100) + 1;
+    setRandomNum(random);
+  };
+
+  
+  
+  useEffect(() => {
+    makeRandom();
+   
+  }, []);
+  
+  
+// InputValue
+  const handleChange = (e) => {
+
+    setUserNum(e.target.value);
+    
+
+  };
+  // Submit Button
+  console.log(msg)
+  const submitBtn = (evt) => {
+    evt.preventDefault();
+    
+  
+    
+  //  Order
+   
+      if (userNum > 100) {
+        setSrc("https://media.giphy.com/media/XJzinpzkMZZo8KcSP5/giphy.gif")
+        setMsg("Please Choose Less Than 101");
+    }
+
+    else if(history.includes(userNum.toString()) ){
+      setSrc("https://media.giphy.com/media/XJzinpzkMZZo8KcSP5/giphy.gif")
+       setMsg('You already have tried this number!!')
+      
+     }
+    
+
+     else if (chance <= 1 ) {
+      setChance(chance - 1);
+      setSrc("https://media.giphy.com/media/FgjKGypLCAety/giphy.gif")
+      setGameOver(true);
+    }
+     else if (randomNum > userNum ) {
+      setChance(chance - 1);
+      setSrc('https://media.giphy.com/media/3625U4ote7MTAaGi9n/giphy.gif')
+      setMsg("up");
+    }
+    else if (randomNum < userNum ) {
+      setChance(chance - 1);
+      setSrc("https://media.giphy.com/media/jOnrjNTgv0RnU8bG80/giphy.gif")
+      setMsg("down!");
+    }
+
+     else {
+      setMsg("winner");
+      setSrc("https://media.giphy.com/media/3o72FcJmLzIdYJdmDe/giphy.gif")
+      setGameOver(true);
+      console.log(gameOver);
+    }
+   
+    history.push(userNum.toString())
+   
+  };
+
+ 
+ 
+// Reset Button
+  const reset = () => {
+    setUserNum("");
+    setRandomNum("");
+    setLess("");
+    setMsg("");
+    setGameOver(false);
+    setChance(5);
+    setHistory([]);
+    setSrc("")
+  };
+
+  console.log(less);
+  console.log(history)
+  return (
+    <div className='Play'>
+      
+      <h2>remaining chance:{chance}</h2>
+      <img src={src}/>
+     
+      <h2>{msg}</h2>
+
+      <form className='Play-form' onSubmit={submitBtn}>
+        <input className='Play-input' type="number" onChange={handleChange} ref={focusInput} />
+        {gameOver !== true ? <button className='Play-submit' onClick={makeFocus}>Go</button> : ""}
+      </form>
+      <button className='Play-reset' onClick={reset}>Reset</button>
+      <p>*5번의 기회안에 1부터100까지 번호중 정답을 맞춰주세요!!*</p>
+      <p>*you can guess the correct number between 1 and 100 in five attempts.* </p>
+    </div>
+  );
+};
+
+export default Play;
